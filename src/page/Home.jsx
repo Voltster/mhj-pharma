947b62775319930bb839bc7723da52522ce43fdf
voltster <voltster95@gmail.com>
2024-09-18 15:36:26 +0530

    update icane

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -2,7 +2,6 @@ import Hero from "../components/home/Hero";
 import Card from "../components/home/Card";
 import SectionLayout from "../components/common/SectionLayout";
 import CompanyDetails from "../components/home/CompanyDetails";
-import ourDedication from "../assets/ourDedication.png";
 import Contact from "../components/home/Contact";
 import InfinityScroll from "../components/common/InfinityScroll";
 import Products from "../components/common/Products";
@@ -10,7 +9,7 @@ import indiaFlag from "../assets/IndianFlag.png";
 import usaFlag from "../assets/UsaFlag.png";
 import UkFlag from "../assets/UkFlag.png";
 import sample2 from "../assets/Packshot/Mhj/gapine.png";
-import sample3 from "../assets/Packshot/Mhj/icane.png";
+// import sample3 from "../assets/Packshot/Mhj/icane.png";
 import sample4 from "../assets/Packshot/Mhj/merrin.png";
 import sample5 from "../assets/Packshot/Mhj/timoc.png";
 import sample6 from "../assets/Packshot/Mhj/valpoc.png";
@@ -20,7 +19,7 @@ import { motion as m } from "framer-motion";
 
 const imageData = [
   { img: sample2 },
-  { img: sample3 },
+  // { img: sample3 },
   { img: sample4 },
   { img: sample5 },
   { img: sample6 },
